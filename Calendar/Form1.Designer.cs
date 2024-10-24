5c34add862926fc6c689fb755ab3e65143105e4f
Laura Hatherly <18LHatherly@heckgrammar.co.uk>
2024-10-24 19:44:25 +0100

    added loadsssss of stuff

--- Calendar/Form1.Designer.cs
+++ Calendar/Form1.Designer.cs
@@ -40,6 +40,17 @@
             wednesday = new DataGridViewTextBoxColumn();
             thursday = new DataGridViewTextBoxColumn();
             friday = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            monthCalendar1 = new MonthCalendar();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             Navbar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -51,23 +62,23 @@
             Navbar.Controls.Add(meetingsBtn);
             Navbar.Controls.Add(calendarBtn);
             Navbar.Controls.Add(dashboardBtn);
-            Navbar.Location = new Point(6, 9);
+            Navbar.Location = new Point(7, 7);
             Navbar.Margin = new Padding(6);
             Navbar.Name = "Navbar";
-            Navbar.Size = new Size(371, 949);
+            Navbar.Size = new Size(371, 1469);
             Navbar.TabIndex = 0;
             // 
             // studentsBtn
             // 
-            studentsBtn.BackColor = Color.Transparent;
+            studentsBtn.BackColor = SystemColors.Control;
             studentsBtn.Font = new Font("Calibri", 10F, FontStyle.Regular, GraphicsUnit.Point);
             studentsBtn.Image = (Image)resources.GetObject("studentsBtn.Image");
             studentsBtn.ImageAlign = ContentAlignment.MiddleLeft;
-            studentsBtn.Location = new Point(-1, 466);
+            studentsBtn.Location = new Point(-2, 464);
             studentsBtn.Margin = new Padding(6);
             studentsBtn.Name = "studentsBtn";
             studentsBtn.Padding = new Padding(10, 0, 0, 0);
-            studentsBtn.Size = new Size(373, 73);
+            studentsBtn.Size = new Size(381, 73);
             studentsBtn.TabIndex = 2;
             studentsBtn.Text = "Students";
             studentsBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -76,15 +87,15 @@
             // 
             // meetingsBtn
             // 
-            meetingsBtn.BackColor = Color.Transparent;
+            meetingsBtn.BackColor = SystemColors.Control;
             meetingsBtn.Font = new Font("Calibri", 10F, FontStyle.Regular, GraphicsUnit.Point);
             meetingsBtn.Image = (Image)resources.GetObject("meetingsBtn.Image");
             meetingsBtn.ImageAlign = ContentAlignment.MiddleLeft;
-            meetingsBtn.Location = new Point(-1, 396);
+            meetingsBtn.Location = new Point(-7, 393);
             meetingsBtn.Margin = new Padding(6);
             meetingsBtn.Name = "meetingsBtn";
             meetingsBtn.Padding = new Padding(10, 0, 0, 0);
-            meetingsBtn.Size = new Size(373, 73);
+            meetingsBtn.Size = new Size(380, 73);
             meetingsBtn.TabIndex = 2;
             meetingsBtn.Text = "Meetings";
             meetingsBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -93,15 +104,15 @@
             // 
             // calendarBtn
             // 
-            calendarBtn.BackColor = Color.Transparent;
+            calendarBtn.BackColor = Color.MediumPurple;
             calendarBtn.Font = new Font("Calibri", 10F, FontStyle.Regular, GraphicsUnit.Point);
             calendarBtn.Image = (Image)resources.GetObject("calendarBtn.Image");
             calendarBtn.ImageAlign = ContentAlignment.MiddleLeft;
-            calendarBtn.Location = new Point(-1, 325);
+            calendarBtn.Location = new Point(-3, 323);
             calendarBtn.Margin = new Padding(6);
             calendarBtn.Name = "calendarBtn";
             calendarBtn.Padding = new Padding(10, 0, 0, 0);
-            calendarBtn.Size = new Size(373, 73);
+            calendarBtn.Size = new Size(382, 73);
             calendarBtn.TabIndex = 1;
             calendarBtn.Text = "Calendar";
             calendarBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -111,15 +122,15 @@
             // 
             // dashboardBtn
             // 
-            dashboardBtn.BackColor = Color.Transparent;
+            dashboardBtn.BackColor = SystemColors.Control;
             dashboardBtn.Font = new Font("Calibri", 10F, FontStyle.Regular, GraphicsUnit.Point);
             dashboardBtn.Image = (Image)resources.GetObject("dashboardBtn.Image");
             dashboardBtn.ImageAlign = ContentAlignment.MiddleLeft;
-            dashboardBtn.Location = new Point(-1, 254);
+            dashboardBtn.Location = new Point(-6, 255);
             dashboardBtn.Margin = new Padding(6);
             dashboardBtn.Name = "dashboardBtn";
             dashboardBtn.Padding = new Padding(10, 0, 0, 0);
-            dashboardBtn.Size = new Size(373, 73);
+            dashboardBtn.Size = new Size(380, 73);
             dashboardBtn.TabIndex = 0;
             dashboardBtn.Text = "Dashboard";
             dashboardBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -131,53 +142,189 @@
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Monday, tuesday, wednesday, thursday, friday });
-            dataGridView1.Location = new Point(746, 170);
+            dataGridView1.Location = new Point(896, 192);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 82;
-            dataGridView1.RowTemplate.Height = 41;
-            dataGridView1.Size = new Size(1089, 704);
+            dataGridView1.RowTemplate.Height = 100;
+            dataGridView1.Size = new Size(1745, 965);
             dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // Monday
             // 
             Monday.HeaderText = "Monday";
             Monday.MinimumWidth = 10;
             Monday.Name = "Monday";
-            Monday.Width = 200;
+            Monday.Width = 300;
             // 
             // tuesday
             // 
             tuesday.HeaderText = "Tuesday";
             tuesday.MinimumWidth = 10;
             tuesday.Name = "tuesday";
-            tuesday.Width = 200;
+            tuesday.Width = 300;
             // 
             // wednesday
             // 
             wednesday.HeaderText = "Wednesday";
             wednesday.MinimumWidth = 10;
             wednesday.Name = "wednesday";
-            wednesday.Width = 200;
+            wednesday.Width = 300;
             // 
             // thursday
             // 
             thursday.HeaderText = "Thursday";
             thursday.MinimumWidth = 10;
             thursday.Name = "thursday";
-            thursday.Width = 200;
+            thursday.Width = 300;
             // 
             // friday
             // 
             friday.HeaderText = "Friday";
             friday.MinimumWidth = 10;
             friday.Name = "friday";
-            friday.Width = 200;
+            friday.Width = 300;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Consolas", 19.875F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Indigo;
+            label1.Location = new Point(896, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(433, 62);
+            label1.TabIndex = 2;
+            label1.Text = "Laura Hatherly";
+            label1.Click += label1_Click;
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(433, 61);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Miss Brown", "Mr Honey", "Ms Moses", "Mr Moss", "Mr Jones" });
+            comboBox1.Location = new Point(432, 492);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(404, 40);
+            comboBox1.TabIndex = 4;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveBorder;
+            label2.ForeColor = SystemColors.Desktop;
+            label2.Location = new Point(435, 420);
+            label2.Name = "label2";
+            label2.Padding = new Padding(75, 10, 75, 10);
+            label2.Size = new Size(397, 52);
+            label2.TabIndex = 5;
+            label2.Text = "See teacher timetable";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveBorder;
+            label3.ForeColor = SystemColors.Desktop;
+            label3.Location = new Point(450, 552);
+            label3.Name = "label3";
+            label3.Padding = new Padding(120, 10, 120, 10);
+            label3.Size = new Size(367, 52);
+            label3.TabIndex = 6;
+            label3.Text = "Categories";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Brown;
+            label4.ForeColor = SystemColors.Desktop;
+            label4.Location = new Point(450, 629);
+            label4.Name = "label4";
+            label4.Padding = new Padding(130, 10, 130, 10);
+            label4.Size = new Size(369, 52);
+            label4.TabIndex = 7;
+            label4.Text = "Priority 1";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Goldenrod;
+            label5.ForeColor = SystemColors.Desktop;
+            label5.Location = new Point(450, 703);
+            label5.Name = "label5";
+            label5.Padding = new Padding(130, 10, 130, 10);
+            label5.Size = new Size(369, 52);
+            label5.TabIndex = 8;
+            label5.Text = "Priority 2";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.ForestGreen;
+            label6.ForeColor = SystemColors.Desktop;
+            label6.Location = new Point(450, 780);
+            label6.Name = "label6";
+            label6.Padding = new Padding(130, 10, 130, 10);
+            label6.Size = new Size(369, 52);
+            label6.TabIndex = 9;
+            label6.Text = "Priority 3";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.MediumPurple;
+            label7.ForeColor = SystemColors.Desktop;
+            label7.Location = new Point(451, 858);
+            label7.Name = "label7";
+            label7.Padding = new Padding(105, 10, 105, 10);
+            label7.Size = new Size(368, 52);
+            label7.TabIndex = 10;
+            label7.Text = "Staff meeting";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = SystemColors.ControlDarkDark;
+            label8.ForeColor = SystemColors.Desktop;
+            label8.Location = new Point(450, 938);
+            label8.Name = "label8";
+            label8.Padding = new Padding(150, 10, 150, 10);
+            label8.Size = new Size(375, 52);
+            label8.TabIndex = 11;
+            label8.Text = "Other";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = SystemColors.ActiveBorder;
+            label9.ForeColor = SystemColors.Desktop;
+            label9.Location = new Point(448, 1027);
+            label9.Name = "label9";
+            label9.Padding = new Padding(150, 10, 150, 10);
+            label9.Size = new Size(377, 52);
+            label9.TabIndex = 12;
+            label9.Text = "Guide";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1993, 960);
+            ClientSize = new Size(2734, 1453);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(label1);
             Controls.Add(dataGridView1);
             Controls.Add(Navbar);
             Margin = new Padding(6);
@@ -187,6 +334,7 @@
             Navbar.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -197,10 +345,21 @@
         private Button studentsBtn;
         private Button meetingsBtn;
         private DataGridView dataGridView1;
+        private Label label1;
+        private MonthCalendar monthCalendar1;
         private DataGridViewTextBoxColumn Monday;
         private DataGridViewTextBoxColumn tuesday;
         private DataGridViewTextBoxColumn wednesday;
         private DataGridViewTextBoxColumn thursday;
         private DataGridViewTextBoxColumn friday;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
--- Calendar/Form1.cs
+++ Calendar/Form1.cs
@@ -9,8 +9,15 @@ namespace Calendar
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            PopulateDataGridView();
+            this.comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            dataGridView1.RowHeadersWidth = 200;
+            for (int i = 0; i < 5; i++)
+            {
+                this.dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
         }
+
 
         private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
@@ -35,6 +42,133 @@ namespace Calendar
         private void button1_Click_1(object sender, EventArgs e)
         {
 
+        }
+        private void PopulateDataGridView()
+        {
+            string[] rowb4school = { " ", " ", " ",
+            " ", " " };
+            string[] row0 = { " ", "Meeting Astrid", " ",
+            "Lesson", "Lesson" };
+            string[] rowbreak = { "Break", "Break", "Break",
+            "Break", "Break" };
+            string[] row1 = { "Lesson", " ", "Meeting Zaryaab",
+            " ", "Personal profile meeting" };
+            string[] row2 = { "Training day", " ", "Lesson",
+            "Lesson", "Meeting Astrid" };
+            string[] rowlunch = { "Lunch", "Lunch", "Lunch",
+            "Lunch", "Lunch" };
+            string[] row3 = { " ", "Lesson", "Meeting Thomas",
+            " ", "Teacher meeting" };
+            string[] row4 = { "Meeting Chloe", " ", "Lesson",
+            " ", "Psychedelic Jungle party" };
+            string[] rowafterschool = { " ", " ", " ",
+            " ", " " };
+
+            dataGridView1.Rows.Add(rowb4school);
+            dataGridView1.Rows.Add(row0);
+            dataGridView1.Rows.Add(rowbreak);
+            dataGridView1.Rows.Add(row1);
+            dataGridView1.Rows.Add(row2);
+            dataGridView1.Rows.Add(rowlunch);
+            dataGridView1.Rows.Add(row3);
+            dataGridView1.Rows.Add(row4);
+            dataGridView1.Rows.Add(rowafterschool);
+
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.Transparent;
+            DateTime localDate = DateTime.Now;
+            int dayOfWeek = Convert.ToInt32(localDate.DayOfWeek);
+            string time = Convert.ToString(localDate.ToShortTimeString());
+            int totalTime = 0;
+            int timeIndex = 0;
+
+
+            totalTime = (Convert.ToInt32(time.Substring(0, 2)) * 100) + Convert.ToInt32(time.Substring(3, 2));
+
+            //MessageBox.Show(Convert.ToString(totalTime));
+            if (totalTime < 900)
+            {
+                timeIndex = 0;
+            }
+            if (totalTime < 1000 && totalTime > 900)
+            {
+                timeIndex = 1;
+            }
+            else if (totalTime < 1020 && totalTime > 1000)
+            {
+                timeIndex = 2;
+            }
+            else if (totalTime < 1120 && totalTime > 1020)
+            {
+                timeIndex = 3;
+            }
+            else if (totalTime < 1220 && totalTime > 1120)
+            {
+                timeIndex = 4;
+            }
+            else if (totalTime < 1315 && totalTime > 1220)
+            {
+                timeIndex = 5;
+            }
+            else if (totalTime < 1415 && totalTime > 1315)
+            {
+                timeIndex = 6;
+            }
+            else if (totalTime < 1515 && totalTime > 1415)
+            {
+                timeIndex = 7;
+            }
+            else if (totalTime < 1745 && totalTime > 1515)
+            {
+                timeIndex = 8;
+            }
+            else
+            {
+                timeIndex = -1;
+            }
+
+            if (timeIndex == -1)
+            {
+                dataGridView1.Rows[0].Cells[0].Style.BackColor = Color.White;
+            }
+            else
+            {
+                dataGridView1.Rows[timeIndex].Cells[dayOfWeek - 1].Style.BackColor = Color.LightGray;
+            }
+
+
+
+            // for (int i = 0; i < this.dataGridView1.Rows.Count; i++)
+            // {
+            //    dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            //}
+            dataGridView1.Rows[0].HeaderCell.Value = "Before School";
+            dataGridView1.Rows[1].HeaderCell.Value = "Period 1";
+            dataGridView1.Rows[2].HeaderCell.Value = "Break";
+            dataGridView1.Rows[3].HeaderCell.Value = "Period 2";
+            dataGridView1.Rows[4].HeaderCell.Value = "Period 3";
+            dataGridView1.Rows[5].HeaderCell.Value = "Lunch";
+            dataGridView1.Rows[6].HeaderCell.Value = "Period 4";
+            dataGridView1.Rows[7].HeaderCell.Value = "Period 5";
+            dataGridView1.Rows[8].HeaderCell.Value = "After School";
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
